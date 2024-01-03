44b75b09fc15e6c96275301676fa73e9d95da747
Vladimir <homie43@yandex.ru>
2024-01-03 11:37:36 +0300

    made a visual category selection

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,11 +1,25 @@
+import React from 'react';
+
+const categories = ['Все', 'Классические', 'Запечёные', 'Вегетаринаские'];
+
 const Categories = () => {
+    const [activeIndex, setActiveIndex] = React.useState(0);
+
+    const onClickCategory = (index: number) => {
+        setActiveIndex(index);
+    };
     return (
         <div className='categories'>
             <ul>
-                <li className='active'>Все</li>
-                <li>Классические</li>
-                <li>Запечёные</li>
-                <li>Вегетаринаские</li>
+                {categories.map((item, index) => (
+                    <li
+                        className={activeIndex === index ? 'active' : ''}
+                        key={index}
+                        onClick={() => onClickCategory(index)}
+                    >
+                        {item}
+                    </li>
+                ))}
             </ul>
         </div>
     );
