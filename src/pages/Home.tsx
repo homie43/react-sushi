ed959d5cfb075a57bea1c43afcf5e05cc0713527
Vladimir <homie43@yandex.ru>
2024-01-03 17:31:48 +0300

    deleted local data, downloaded data from  server

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,27 @@
+import React from 'react';
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import SushiBlock from '../components/SushiBlock';
-import sushis from '../assets/db.json';
+import axios from 'axios';
 
 const Home = () => {
+    const [items, setItems] = React.useState([]);
+
+    React.useEffect(() => {
+        async function fetchData() {
+            try {
+                const itemsRespone = await axios.get(
+                    'https://79468735656e78dd.mokky.dev/items'
+                );
+                setItems(itemsRespone.data);
+            } catch (error) {
+                console.log('Ошибка при запросе данных');
+                console.log(error);
+            }
+        }
+        fetchData();
+    }, []);
+
     return (
         <div className='container'>
             <div className='content__top'>
@@ -12,16 +30,16 @@ const Home = () => {
             </div>
             <h2 className='content__title'>Все роллы</h2>
             <div className='content__items'>
-                {sushis.map((obj) => (
+                {items.map((obj) => (
                     <SushiBlock
                         key={obj.id}
-                        // title={obj.title}
-                        // imageUrl={obj.imageUrl}
-                        // describe={obj.describe}
-                        // price={obj.price}
+                        title={obj.title}
+                        imageUrl={obj.imageUrl}
+                        describe={obj.describe}
+                        price={obj.price}
                         // если я уверен что в компоненте будут все свойства, что я описал выше
                         // то передаю просто с помощуью спред оператора
-                        {...obj}
+                        // {...obj}
                     />
                 ))}
             </div>
