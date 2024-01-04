3c8f6d9c5fb4838cea4017d98bb052e05bd24129
Vladimir <homie43@yandex.ru>
2024-01-04 13:24:59 +0300

    add react content loader, add loader-skeleton

---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const Skeleton = () => (
+    <ContentLoader
+        speed={0}
+        width={230}
+        height={387}
+        viewBox='0 0 230 387'
+        backgroundColor='#f3f3f3'
+        foregroundColor='#ecebeb'
+    >
+        <rect x='550' y='569' rx='3' ry='3' width='67' height='11' />
+        <rect x='474' y='36' rx='10' ry='10' width='150' height='132' />
+        <rect x='627' y='109' rx='3' ry='3' width='150' height='15' />
+        <rect x='618' y='-278' rx='3' ry='3' width='93' height='15' />
+        <rect x='337' y='113' rx='9' ry='9' width='80' height='24' />
+        <rect x='5' y='399' rx='0' ry='0' width='130' height='36' />
+        <rect x='231' y='364' rx='0' ry='0' width='43' height='37' />
+        <rect x='6' y='5' rx='5' ry='5' width='220' height='204' />
+        <rect x='17' y='264' rx='5' ry='5' width='172' height='43' />
+        <rect x='25' y='348' rx='5' ry='5' width='55' height='19' />
+        <rect x='95' y='342' rx='5' ry='5' width='124' height='35' />
+        <rect x='16' y='222' rx='5' ry='5' width='194' height='31' />
+    </ContentLoader>
+);
+
+export default Skeleton;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import SushiBlock from '../components/SushiBlock';
 import axios from 'axios';
+import Skeleton from '../components/Skeleton';
 
 export interface Sushi {
     id: number;
@@ -16,21 +17,43 @@ export interface Sushi {
 
 const Home = () => {
     const [items, setItems] = React.useState<Sushi[]>([]);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [isLoading, setIsLoading] = React.useState(false);
 
     React.useEffect(() => {
         async function fetchData() {
             try {
+                setIsLoading(true);
                 const itemsRespone = await axios.get(
                     'https://79468735656e78dd.mokky.dev/items'
                 );
                 setItems(itemsRespone.data);
+                setIsLoading(false);
             } catch (error) {
                 console.log('Ошибка при запросе данных');
                 console.log(error);
+                setIsLoading(true);
             }
         }
         fetchData();
-    }, []);
+    }, []); // при первом рендере отрабоает useEffect
+
+    const skeletons = [...new Array(4)].map((_, index) => (
+        <Skeleton key={index} />
+    )); // рендер фейкового массива
+
+    const sushis = items.map((sushi) => (
+        <SushiBlock
+            key={sushi.id}
+            // title={sushi.title}
+            // imageUrl={sushi.imageUrl}
+            // describe={sushi.describe}
+            // price={sushi.price}
+            // если я уверен что в компоненте будут все свойства, что я описал выше
+            // то передаю просто с помощуью спред оператора
+            {...sushi}
+        />
+    ));
 
     return (
         <div className='container'>
@@ -40,18 +63,7 @@ const Home = () => {
             </div>
             <h2 className='content__title'>Все роллы</h2>
             <div className='content__items'>
-                {items.map((sushi) => (
-                    <SushiBlock
-                        key={sushi.id}
-                        // title={sushi.title}
-                        // imageUrl={sushi.imageUrl}
-                        // describe={sushi.describe}
-                        // price={sushi.price}
-                        // если я уверен что в компоненте будут все свойства, что я описал выше
-                        // то передаю просто с помощуью спред оператора
-                        {...sushi}
-                    />
-                ))}
+                {isLoading ? skeletons : sushis}
             </div>
         </div>
     );
